4660bc02c2a2b161c103045550ac5e79f1dfa220
Adnanarodiya <aiarodiya07@gmail.com>
2023-03-18 12:47:14 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-
-import Login from "./components/Login";
-import Home from "./components/Home";
+import CoustomeButton from "./components/button";
+import Navbar from "./components/Home";
 
 function App() {
-  return <Home />;
+  return <Navbar />;
 }
 
 export default App;
