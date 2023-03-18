fe7d325e28d1bda22929e9864b6c84a0cc0becb0
Tejash Patel <pateltejash429@gmail.com>
2023-03-18 12:42:21 +0530

    Push

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
-import Login from "./components/Login";
-import Home from "./components/Home";
+import Home from "./Pages/Home";
+import login from "./Pages/Login";
 
 function App() {
-  return <Home />;
+  return <login />;
 }
 
 export default App;
